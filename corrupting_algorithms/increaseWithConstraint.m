5f5573f23d3ad9224fd4a0b0ef04d5830d8a3ad2
SimoneSantoro21 <simosant2112@gmail.com>
2023-12-12 16:14:52 +0100

    Updating corrupting algorithms

---
+++ corrupting_algorithms/increaseWithConstraint.m
@@ -0,0 +1,11 @@
+function updated_matrix = increaseWithConstraint(matrix)
+    [rows, cols] = size(matrix);
+    updated_matrix = matrix;
+    for col = 1:cols
+        increment = ((col - 1)/cols)*200;
+        column_values = matrix(:, col) - round(increment);
+        column_values(column_values > 255) = 255; % Applying the constraint
+        column_values(column_values < 0) = 0; % Applying the constraint
+        updated_matrix(:, col) = column_values;
+    end
+end
