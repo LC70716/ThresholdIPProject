a751134550a81afeedffc48514726740ec517394
LC70716 <contileonardo59@gmail.com>
2023-12-12 17:50:49 +0100

    minor optimization

--- zhang_threshold/zhang_threshold.m
+++ zhang_threshold/zhang_threshold.m
@@ -1,19 +1,23 @@
 function thresholded = zhang_threshold(matrix,window_size)
     thresholded = ones(size(matrix,1),size(matrix,2)) * 255; % initializing output as all background
     for row = 1 : size(matrix,1) %iterating over all rows and colums
-       accepted_points = []; %points which actually lie inside the image
         for col = 1 : size(matrix,2)
+            accepted_points_to_reshape = zeros(1,window_size^2); %points which actually lie inside the image
+            index_to_modify = 1; %avoiding multiple resizing of accepted_points
             for w = -round(window_size/2) : round(window_size/2)
-                if row + w > 0 && row + w <= size(matrix:1) 
-                   if col + w > 0 && col + w <= size(matrix:2)
-                      accepted_points(end + 1) = matrix(row+w,column+w); % if the point is inside the image then it is accepted
+                if row + w > 0 && row + w <= size(matrix,1) 
+                   if col + w > 0 && col + w <= size(matrix,2)
+                      accepted_points_to_reshape(index_to_modify) = matrix(row+w,col+w); % if the point is inside the image then it is accepted
+                      index_to_modify = index_to_modify + 1;
                    end                     
                 end     
             end
+        accepted_points = accepted_points_to_reshape(1:index_to_modify);
         mean_ = mean(accepted_points);
         std_dev = std(accepted_points);
         [count,~] = groupcounts(accepted_points); %needed to compute the probability of each gray level
-        entropy_ = -1 * sum(count/size(accepted_points)*log2(count/size(accepted_points)));
+        count = count./size(accepted_points); %computing the probabilities
+        entropy_ = -1 * sum(count.*log2(count));
         threshold = mean_ - (entropy_^2/std_dev);
         if matrix(row,col) < threshold
             thresholded(row,col) = 0;
