4dd5fc1d82f8e395196a7a80e7d471aa4b02ae14
LC70716 <contileonardo59@gmail.com>
2023-12-14 15:17:12 +0100

    code refactoring, added batch method for computation of best parameters, added best_parameters_niblack

---
+++ niblack_threshold/best_parameters_niblack.m
@@ -0,0 +1,71 @@
+function [w_array,k_array,psnr_vals,ssim_vals,mssim_vals] = best_parameters_niblack(corrupt, gr_truth)
+    j = 0; % window index
+    psnr_vals = zeros(1, 10); % Preallocate arrays to store metric values
+    ssim_vals = zeros(1, 10);
+    mssim_vals = zeros(1, 10);
+    w_array = zeros(1, 10);
+    k_array = zeros(1, 40);
+
+    for window = 5:10:105 % window size for loop
+        j = j + 1;
+        w_array(j) = window;
+
+        k = 1; % Start value for bias index
+
+        for bias_k = 0:0.025:1 % bias for loop
+            % CALLING NIBLACK THRESHOLD FUNCTION
+            thresholded = niblack(corrupt, [window window], bias_k);
+
+            % Computing metrics
+            ssim_val = ssim(thresholded, gr_truth);
+            psnr_val = psnr(thresholded, gr_truth);
+            mssim_val = multissim(thresholded, gr_truth);
+            
+            % Storing metric values for each window size and bias
+            ssim_vals(k, j) = ssim_val;
+            psnr_vals(k, j) = psnr_val;
+            mssim_vals(k, j) = mssim_val;
+            
+            k = k + 1; % Increment the bias index
+        end
+    end
+
+    % Finding maximum PSNR and SSIM values and their corresponding window sizes
+    [max_psnr, index_psnr] = max(psnr_vals(:));
+    [max_ssim, index_ssim] = max(ssim_vals(:));
+    [max_mssim, index_mssim] = max(mssim_vals(:));
+
+    % Finding the corresponding indices for max values
+    [row_psnr, col_psnr] = ind2sub(size(psnr_vals), index_psnr);
+    [row_ssim, col_ssim] = ind2sub(size(ssim_vals), index_ssim);
+    [row_mssim, col_mssim] = ind2sub(size(mssim_vals), index_mssim);
+
+    disp('Max PSNR:');
+    disp(max_psnr);
+    disp('Window size and bias for max PSNR:');
+    disp([w_array(col_psnr), k_array(row_psnr)]);
+
+    disp('Max SSIM:');
+    disp(max_ssim);
+    disp('Window size and bias for max SSIM:');
+    disp([w_array(col_ssim), k_array(row_ssim)]);
+
+    disp('Max MSSIM:');
+    disp(max_mssim);
+    disp('Window size and bias for max MSSIM:');
+    disp([w_array(col_mssim), k_array(row_mssim)]);
+
+    % Plotting PSNR vs. Window
+    figure;
+    plot(w_array, psnr_vals, '-o', 'LineWidth', 2);
+    xlabel('Window Size');
+    ylabel('PSNR');
+    title('PSNR vs. Window Size');
+
+    % Plotting SSIM vs. Window
+    figure;
+    plot(w_array, ssim_vals, '-o', 'LineWidth', 2);
+    xlabel('Window Size');
+    ylabel('SSIM');
+    title('SSIM vs. Window Size');
+end
---
+++ parameter_computation/batch_best_parameters.m
@@ -0,0 +1,22 @@
+% returns window sizes 2D matrix, bias (k) 2D matrix, and a list of matrices (basically 3D matrix) for each parameter. 
+% The FIRST index relates to the ALGORITHM USED, the SECOND to THE IMAGE
+% ref_images and corrupted_images ARE INTENDED TO BE ARRAYS CONTAINING THE IMAGES' NAMES in such fashion : 'myimage.tiff'
+% make sure to add the WHOLE REPO to your matlab path
+% since ZHANG doesn't have bias, they are all listed as NaN, 
+% if in the future algos have more or less arguments the same approach should be used to detect problems during data analysis
+function [w_matrix,k_matrix,psnr_vals,ssim_vals,mssim_vals] = batch_best_parameters(ref_images,cor_images)
+    w_matrix = zeros(3,10); % 3 algorithms, so first index has length 3. 10 different window sizes,so second index has length 10.
+    k_matrix = zeros(3,40);
+    psnr_vals = zeros(3,size(ref_images));
+    ssim_vals = zeros(3,size(ref_images));
+    mssim_vals = zeros(3,size(ref_images));
+    for i = 1 : size(ref_images) 
+        [w_matrix(1,i),k_matrix(1,i),psnr_vals(1,i),ssim_vals(1,i),pmssim_vals(1,i)] = best_parameters_singh(cor_images(i),ref_images(i));
+    end
+    for i = 1 : size(ref_images)
+        [w_matrix(2,i),k_matrix(2,i),psnr_vals(2,i),ssim_vals(2,i),pmssim_vals(2,i)] = best_parameters_zhang(cor_images(i),ref_images(i));
+    end 
+    for i = 1 : size(ref_images)
+        [w_matrix(3,i),k_matrix(3,i),psnr_vals(3,i),ssim_vals(3,i),pmssim_vals(3,i)] = best_parameters_niblack(cor_images(i),ref_images(i));
+    end
+
