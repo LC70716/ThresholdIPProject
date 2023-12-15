e602b215e7b7179a076e37d1b809aa38dcd5cf53
LC70716 <contileonardo59@gmail.com>
2023-12-15 23:01:51 +0100

    completion of best param phansalkar

--- phankalsar_threshold/best_parameters_phansalkar.m
+++ phankalsar_threshold/best_parameters_phansalkar.m
@@ -1,4 +1,4 @@
-function [w_array,k_array,R_array,p_array,psnr_vals,ssim_vals,mssim_vals] = best_parameters_phansalkar(corrupt_list, gr_truth_list)
+function [w_array,k_array,R_array,p_array,psnr_vals2,ssim_vals2,mssim_vals2] = best_parameters_phansalkar(corrupt_list, gr_truth_list)
     j = 0; % window index
     psnr_vals = zeros(41, 11); % Preallocate arrays to store metric values
     psnr_vals2 = cell([41 41]);
@@ -8,9 +8,13 @@ function [w_array,k_array,R_array,p_array,psnr_vals,ssim_vals,mssim_vals] = best
     mssim_vals2 = cell([41 41]);
     w_array = zeros(1, 11);
     k_array = zeros(1, 41);
-    R_array = zeros(1,41);
-    p_array = zeros(1,41);
+    R_array = zeros(1, 41);
+    p_array = zeros(1, 41);
 
+    for i = 1 : size(corrupt_list)
+    
+        corrupt = imread(corrupt_list(i));
+        gr = imread(gr_truth_list(i));
 
     for window = 5:10:105 % window size for loop
         j
@@ -23,26 +27,31 @@ function [w_array,k_array,R_array,p_array,psnr_vals,ssim_vals,mssim_vals] = best
             k = k + 1; % Increment the bias index
             R = 1;   %Start value for R index
 
-            for R_param = 0:0:0
+            for R_param = 0:0.025:1
                 p = 1;   %Start value for p index
-                for p_param = 0:0:0
+                for p_param = 0:0.15:6
                     % CALLING SINGH THRESHOLD FUNCTION
-                    thresholded = phansalkar_threshold(corrupt_list(i), window, bias_k, R_param, p_param);
+                    thresholded = phansalkar_threshold(corrupt, window, bias_k, R_param, p_param);
 
                     % Computing metrics
-                    ssim_val = ssim(thresholded, gr_truth_list);
-                    psnr_val = psnr(thresholded, gr_truth_list);
-                    mssim_val = multissim(thresholded, gr_truth_list);
+                    ssim_val = ssim(thresholded, gr);
+                    psnr_val = psnr(thresholded, gr);
+                    mssim_val = multissim(thresholded, gr);
 
                     % Storing metric values for each window size and bias
                     ssim_vals(R, p) = ssim_val;
                     psnr_vals(R, p) = psnr_val;
                     mssim_vals(R, p) = mssim_val;
+
+                    p = p + 1;
                 end
+                R = R + 1;
             end
 
             ssim_vals2{w}{k} = ssim_vals;
             psnr_vals2{w}{k} = psnr_vals;
             mssim_vals2{w}{k} = mssim_vals;
+
         end
+    end
     end
