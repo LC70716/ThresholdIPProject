3b910be58b037616a2462c53b4e589144a3cccad
LC70716 <contileonardo59@gmail.com>
2023-12-15 18:47:40 +0100

    added best_parameters_bersen, ready for first trial

--- Bernsen_thresholding/best_parameters_bernsen.m
+++ Bernsen_thresholding/best_parameters_bernsen.m
@@ -1,21 +1,20 @@
-function [w_array,k_array,psnr_vals,ssim_vals,mssim_vals] = best_parameters_sauvola(corrupt, gr_truth)
+function [w_array,k_array,psnr_vals,ssim_vals,mssim_vals] = best_parameters_bernsen(corrupt, gr_truth)
     j = 0; % window index
     psnr_vals = zeros(40, 10); % Preallocate arrays to store metric values
     ssim_vals = zeros(40, 10);
     mssim_vals = zeros(40, 10);
-    w_array = zeros(1, 10);
-    k_array = zeros(1, 40);
+    w_array = zeros(40, 10);
+    k_array = zeros(40, 40);
 
     for window = 5:10:105 % window size for loop
-        j
         j = j + 1;
         w_array(j) = window;
 
         k = 1; % Start value for bias index
 
-        for bias_k = 0:0.025:1 % bias for loop
-            % CALLING SAUVOLA THRESHOLD FUNCTION
-            thresholded = sauvola_threshold(corrupt, window, bias_k);
+        for bias_k = 0:5:200 % bias for loop
+            % CALLING BERSNEN THRESHOLD FUNCTION
+            thresholded = bernsen_threshold(corrupt, [window window], bias_k);
 
             % Computing metrics
             ssim_val = ssim(thresholded, gr_truth);
--- parameter_computation/batch_best_parameters.m
+++ parameter_computation/batch_best_parameters.m
@@ -2,14 +2,14 @@
 % The FIRST index relates to the ALGORITHM USED, the SECOND to THE IMAGE
 % ref_images and corrupted_images ARE INTENDED TO BE ARRAYS CONTAINING THE IMAGES' NAMES in such fashion : 'myimage.tiff'
 % make sure to add the WHOLE REPO to your matlab path
-% since ZHANG doesn't have bias, they are all listed as -1, 
-% if in the future algos have more or less arguments the same approach should be used to detect problems during data analysis
+% ZHANG doesn't have bias, they are all listed as -1, 
+% BERNSEN doesn't have bias but has CONTRAST THRESHOLDING, and that's what is listed in its k matrix row
 function [w_matrix,k_matrix,psnr_vals,ssim_vals,mssim_vals] = batch_best_parameters(ref_images,cor_images)
-    w_matrix = zeros(4,10); % 3 algorithms, so first index has length 3. 10 different window sizes,so second index has length 10.
-    k_matrix = zeros(4,40);
-    psnr_vals = zeros(4,size(ref_images));
-    ssim_vals = zeros(4,size(ref_images));
-    mssim_vals = zeros(4,size(ref_images));
+    w_matrix = zeros(5,10); % 3 algorithms, so first index has length 3. 10 different window sizes,so second index has length 10.
+    k_matrix = zeros(5,40);
+    psnr_vals = zeros(5,size(ref_images));
+    ssim_vals = zeros(5,size(ref_images));
+    mssim_vals = zeros(5,size(ref_images));
     for i = 1 : size(ref_images) 
         [w_matrix(1,i),k_matrix(1,i),psnr_vals(1,i),ssim_vals(1,i),pmssim_vals(1,i)] = best_parameters_singh(imread(cor_images(i)),imread(ref_images(i)));
     end 
@@ -19,7 +19,9 @@ function [w_matrix,k_matrix,psnr_vals,ssim_vals,mssim_vals] = batch_best_paramet
     for i = 1 : size(ref_images)
         [w_matrix(3,i),k_matrix(3,i),psnr_vals(3,i),ssim_vals(3,i),pmssim_vals(3,i)] = best_parameters_sauvola(imread(cor_images(i)),imread(ref_images(i)));
     end
-    
+    for i = 1 : size(ref_images)
+        [w_matrix(4,i),k_matrix(4,i),psnr_vals(4,i),ssim_vals(4,i),pmssim_vals(4,i)] = best_parameters_bernsen(imread(cor_images(i)),imread(ref_images(i)));
+    end
     for i = 1 : size(ref_images)
         [w_matrix(5,i),k_matrix(5,i),psnr_vals(5,i),ssim_vals(5,i),pmssim_vals(5,i)] = best_parameters_zhang(imread(cor_images(i)),imread(ref_images(i)));
     end
